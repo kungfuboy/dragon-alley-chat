6e93b0cd492dd30a8ba51320941549c64e49e1dc
songjunjie <songjunjie@advanpro.hk>
2018-09-29 17:05:04 +0800

    deploy

---
+++ precache-manifest.7676b519e55c03b4aa3c6603f6344a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dragon-valley-chat/robots.txt"
+  },
+  {
+    "revision": "7253e3206b7e993947ba",
+    "url": "/dragon-valley-chat/js/chunk-vendors.55fcb56e.js"
+  },
+  {
+    "revision": "525fc009a976cfc44463",
+    "url": "/dragon-valley-chat/js/app.dfa6ddfc.js"
+  },
+  {
+    "revision": "8e4a91da5e9c99f4fad707a54ad4cc06",
+    "url": "/dragon-valley-chat/index.html"
+  },
+  {
+    "revision": "49767ac507f1808d28b1122464ebb4e1",
+    "url": "/dragon-valley-chat/img/texture.49767ac5.png"
+  },
+  {
+    "revision": "525fc009a976cfc44463",
+    "url": "/dragon-valley-chat/css/app.a64c0b58.css"
+  }
+];
